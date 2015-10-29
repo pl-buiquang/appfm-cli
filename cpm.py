8b1052f9bbab8cd964afd2c6eb9da2ceb27f57e9
Paul Bui-Quang <paul.bui-quang@limsi.fr>
2015-10-29 11:56:37 +0100

    added command docs, timeout system, webbrowser result viewer

--- cpm.py
+++ cpm.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python
 
-'''
-Corpus Process Manager CLI
-
-Usage:
-  cpm modules ls [-a]
-  cpm modules run <module_name> <conf_file>
-
-  cpm process ls
-  cpm process <pid> (start | stop | restart | status) 
-  cpm process 
-
-Options:
-  -a -all   Print all modules
-
-'''
-
 import sys
 import zmq
 import os
 
 import click
-
+import webbrowser
 
 from message import com
 
@@ -29,30 +13,70 @@ from message import com
 servername = "localhost"#"rscpm"
 serverport = "5555"
 connectionInfo = "tcp://"+servername+":"+serverport
+guihost = 'http://'+servername+':8080/'
 
 context = zmq.Context()
 
 sock = context.socket(zmq.REQ)
+sock.setsockopt(zmq.LINGER, 0)
 sock.connect(connectionInfo)
 
 @click.group()
 def cli() :
+  """CPM CLI - Corpus & Process Manager Command Line Interface"""
   pass
 
 @cli.command()
 def reload() :
+  """Refresh cpm modules defintion"""
   print com.sendMessage(sock,"reload")
 
 @cli.group()
 def module() :
+  """Manage cpm modules"""
   pass
 
 def modulesls(name):
-  return com.sendMessage(sock,"module ls"+name)
+  return com.sendMessage(sock,"module ls"+name,10)
+  
+def modulehelp(name):
+  return com.sendMessage(sock,"module getdesc "+name,10)
+
+def modulefunc(name):
+  @cli.command(help=modulehelp(name))
+  @click.argument('conf_file')
+  def func(conf_file):
+    print com.sendMessage(sock,"module run "+name+" "+conf_file)
+  return func
+
+
+class CPMModules(click.MultiCommand):
+
+    def list_commands(self, ctx):
+        rv = []
+        modules = modulesls(" --name")
+        for filename in modules.split("\n"):
+            if filename.strip() != "":
+                rv.append(filename)
+        rv.sort()
+        return rv
+
+    def get_command(self, ctx, name):
+        return modulefunc(name)
+
+
+
+
+@cli.command(cls=CPMModules)
+def run():
+  """Shorthand for "module run" command"""
+  pass
+  
 
 @module.command()
-@click.option('--name', default=False,is_flag=True)
+@click.option('--name', default=False,is_flag=True,help="Only print modules' name")
 def ls(name):
+  """List all available modules"""
   optname = ""
   if name :
     optname = " --name"
@@ -60,34 +84,63 @@ def ls(name):
 
 
 
-@module.command()
-@click.argument('module')
-@click.argument('conf_file')
-def run(module,conf_file):
-  print com.sendMessage(sock,"module run "+module+" "+conf_file)
+@module.command(cls=CPMModules)
+def run():
+  """Run a MODULE with configuration file CONF_FILE"""
+  pass
 
 
 @cli.group()
 def process():
+  """Manage cpm process"""
   pass
 
 @process.command()
-@click.option('--all','-a', default=False,is_flag=True)
-def ls(all):
+@click.option('--all','-a', default=False,is_flag=True,help="List all processes (including stopped/finished processes)")
+@click.option('--recursive','-r', default=False,is_flag=True,help="List also children processes")
+def ls(all,recursive):
+  """List running processes"""
   optall = ""
+  optrec = ""
   if all :
     optall = " -a"
-  print com.sendMessage(sock,"process ls"+optall)
+  if recursive :
+    optrec = " -r"
+  print com.sendMessage(sock,"process ls"+optall+optrec)
 
 @process.command()
 @click.argument('pid')
 def status(pid):
+  """Print the status of a process and its children processes if any"""
   print com.sendMessage(sock,"process status "+pid)
 
+@process.command()
+@click.argument('pid')
+@click.argument('outputname')
+@click.option('--gui','-g',default=False,is_flag=True,help="View graphical result in a browser")
+def view(pid,outputname,gui):
+  """View the output OUTPUTNAME of a module run PID"""
+  optgui = ""
+  if gui :
+    optgui = " --gui"
+  res = com.sendMessage(sock,"process view "+pid+" "+outputname+optgui)
+  if gui :
+    webbrowser.open(guihost+url)
+  else :
+    print res
+
+@process.command()
+@click.argument('pid')
+@click.option('--gui','-g',default=False,is_flag=True)
+def log(pid,gui):
+  """View the default error log of a module run PID"""
+  res = com.sendMessage(sock,"process log "+pid)
+  print res
 
 
 if __name__ == '__main__':
-    cli()
+  cli()
+    
 
 
 
--- message/com.py
+++ message/com.py
@@ -2,8 +2,18 @@ import zmq
 import sys
 
 
-def sendMessage(sock,message):
+def sendMessage(sock,message,timeout=-1):
   sock.send_string(message)
-  return sock.recv()
+  if timeout!=-1 :
+    poller = zmq.Poller()
+    poller.register(sock, zmq.POLLIN)
+    if poller.poll(timeout*1000): # 10s timeout in milliseconds
+      msg = sock.recv()
+    else:
+      #raise IOError("Timeout processing cpm request")
+      msg = "Timeout processing cpm request"
+  else :
+    msg = sock.recv()
+  return msg
 
 
