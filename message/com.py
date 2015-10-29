8f1d850626f09acdba7dd43097aee650da434927
Paul Bui-Quang <paul.bui-quang@limsi.fr>
2015-10-29 18:22:21 +0100

    added cpm command objects

--- message/com.py
+++ message/com.py
@@ -1,6 +1,20 @@
 import zmq
 import sys
 
+class CPMCommand :
+
+  def __init__(self,command,data="",user="",password=""):
+    self.command = command
+    self.data = data
+    self.user = user
+    self.password = password
+
+  def toMessage(self):
+    return "==USER=="+self.user+"==END_USER====PSWD=="+self.password+"==END_PSWD====CMD=="+self.command+"==END_CMD====DATA=="+self.data+"==END_DATA=="
+
+  def send(sock,timeout=-1):
+    return sendMessage(sock,self.toMessage(),timeout)
+
 
 def sendMessage(sock,message,timeout=-1):
   sock.send_string(message)
@@ -15,5 +29,9 @@ def sendMessage(sock,message,timeout=-1):
   else :
     msg = sock.recv()
   return msg
+
+def sendCommand(sock,command,timeout=-1):
+  cpmcommand = CPMCommand(command)
+  return sendMessage(sock,cpmcommand.toMessage(),timeout)
 
 
